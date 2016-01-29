af207cbe380783dec891b51f5754de7c7d0d5b3e
Ronando <ronando.long@gmail.com>
2016-01-29 08:09:27 -0800

    Spec pass: It will create a 0 receipt if pizza is Medium size

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,24 @@
 // Business Logic
-// var helloWorld = function(){
-//   return false;
-//
-//   For loops and shit go here
-//   If/else like a madman here
-//
-//  return shit like a mo-fo here
-//
-//   ...whatchyu lookin at bitch? Green those specs and git back to work!
-//  Time fo yo Interface Logic, then suckface when it's all green.
-// };
+function Pizza(pizzaSize, topping, qty) {  // parameters
+  this.pizzaSize = pizzaSize;  // properties
+  this.topping = topping;
+  this.qty = qty;
+}
+
+Pizza.prototype.price = function() {
+  var pizzaPrice = 8;  // small pizza with only cheese topping
+
+  if (this.pizzaSize === "Small") {
+    return (pizzaPrice);
+  } else if (this.pizzaSize === "Medium") {
+    return (pizzaPrice + 2);
+  } else if (this.pizzaSize === "Large") {
+    return (pizzaPrice + 4);
+  } else {
+
+  }
+}
+
 
 
 
@@ -21,17 +30,6 @@
 // User Interface Logic
 // $(document).ready(function() {
 //   $("form#IDselector").submit(function(event) {
-//     // variables and shit go here, bitch!
-//
-//     // link your muthafuckin' variables to yo bad-ass bidness logic function, Yo!
-//
-//     // Make shit happen here...BITCH!
-//
-//     // Muthafuckin' variables connect to your output down here hommie.
-//
-//     // Show me some money, hunny, and reveal that hiddin fine shit you got stashed.
-//
-//     // It's all good baby.'
-//     event.preventDefault();
+
 //   });
 // });
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,32 @@
-// describe('helloWorld', function(){
-//   it("is going to be false", function(){
-//     expect(helloWorld()).to.equal(false);
-//   });
-// });
+describe('Pizza', function(){
+  it("will let user order a large pizza", function(){
+    var testPizza = new Pizza("Large", "Pepperoni", 1)
+    expect(testPizza.pizzaSize).to.equal("Large");
+    expect(testPizza.topping).to.equal("Pepperoni");
+    expect(testPizza.qty).to.equal(1);
+  });
+
+  it("will create a $10 receipt if pizza is Medium size", function() {
+    var testPizza = new Pizza("Medium", "Pepperoni", 1)
+    expect(testPizza.price()).to.equal(10);  // we are looking for price
+  });
+
+  // it("will create a 14")
+
+
+});
+
+// specs
+// constructor
+// it will let user order size
+// it will let user order one topping
+// it will let user order two toppings
+
+
+// user can order a pizza and see the final cost.
+// user to choose toppings and size
+// Create a pizza object constructor with properties for toppings and size.
+// Create a prototype method for the cost of a pizza depending on the selections chosen.
+// ** allow users to order more than one pizza with different toppings
+// ** display the list of pizzas ordered as links that can be clicked for details
+// ** offer a delivery option that then requires address information
